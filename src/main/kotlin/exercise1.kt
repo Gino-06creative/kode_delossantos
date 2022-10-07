4d875d5a8e9b355aa9617315c2d8668ed8dc871f
Gino-06creative / kode_delossantos <gino040822@gmail.com>
2022-10-07 21:10:27 +0800

    added exercise_1 package added Main.kt exercise_1

---
+++ src/main/kotlin/exercise1.kt
@@ -0,0 +1,32 @@
+//ApplicationForEmployment
+var positionYouAreApplyingFor: String =""
+var desiredSalary: String =""
+// personalinformation
+var dateAvailableForWork: String =""
+var lastName: String =""
+var firstName: String=""
+var middle: String=""
+var age: String=""
+var birthDAte: String=""
+var address: String=""
+var city: String=""
+var state: String=""
+var zip: String=""
+var homePhone: String=""
+var cellPhone: String=""
+var emailAddress: String=""
+var socialSecurityNumber: String=""
+var areYouAUsCitizen:String=""
+//Education
+var schoolName: String=""
+var location: String=""
+var yearsAttended: String=""
+var degreeReceived: String=""
+var major: String=""
+
+
+
+
+fun main() {
+
+}
