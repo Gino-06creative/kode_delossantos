71666aa01532e3810a668a835fc560c6bc81b24e
Gino-06creative / kode_delossantos <gino040822@gmail.com>
2022-10-17 16:37:22 +0800

    added exercise_1 package added Main.kt exercise_1

--- src/main/kotlin/Avtivity_1/activity01_d.kt
+++ src/main/kotlin/Avtivity_1/activity01_d.kt
@@ -1,11 +1,13 @@
+
+
+
+
 //Create an application that will accept 5 monetary amounts.
-//After the input is done, the user will be asked “
-// ?”. It will only accept an Integer as input.
+//After the input is done, the user will be asked “Divide the value by how many?”. It will only accept an Integer as input.
 //The total of the 5  input will be divided by the answer in the second question input.
-//Note: Error checking must be done.
+
 
 fun main() {
-
     var amount1: Int? = null
     var amount2: Int? = null
     var amount3: Int? = null
@@ -14,17 +16,9 @@ fun main() {
 
     logger.info("Enter Amount:")
     amount1 = readLine()?.toInt()
-
-    logger.info("Enter Amount:")
-    amount2 = readLine()?.toInt()
-
-    logger.info("Enter Amount:")
+     amount2 = readLine()?.toInt()
     amount3 = readLine()?.toInt()
-
-    logger.info("Enter Amount:")
     amount4 = readLine()?.toInt()
-
-    logger.info("Enter Amount:")
     amount5 = readLine()?.toInt()
     logger.info("Divide the value by how many?")
 
@@ -33,7 +27,30 @@ fun main() {
 
 
 
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
