3b1ef43792a6f45b0bdada06bd8469ee6fc80b87
Gino-06creative / kode_delossantos <gino040822@gmail.com>
2022-10-20 22:08:12 +0800

    added exercise_1 package added Main.kt exercise_1

---
+++ src/main/kotlin/Avtivity_1/activity01_e.kt
@@ -0,0 +1,27 @@
+//fun main() {
+    //Identify 3 uses cases that will efficiently use an Array, ArrayList, Set, Map.
+
+    //Array
+    //case 1. In banana plantation
+    //case 2.  eggs in trays
+    //case 3.  a box of cigarette
+
+
+
+//ArrayList
+    //case 1. list of animals in a zoo
+    // case 2. people watching movie in a theater
+    //case  3. Money in an ATM
+
+
+    //Set.
+    //case 1. customers in an online store
+    //case 2. drinks in a vending machine
+    //case 3. a calendar
+
+    //Map
+     // 1. Attendance with date in an Amusement Park
+    //  2. a dictionary
+    //  3. list of songs to be played in an FM Stations for the week
+
+
